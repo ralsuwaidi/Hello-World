e6e7a07ea6b7f227d99f98d12e772420a4746bb6
Rashed Suwaidi <x.suwaidi@gmail.com>
2020-05-07 09:07:52 +0400

    test chart

--- main.py
+++ main.py
@@ -1,19 +1,126 @@
-from PySide2.QtWidgets import QApplication
+import os
+import sys
+import time
+from PySide2 import QtCore, QtWidgets, QtQml
 
-from PySide2.QtQuick import QQuickView
+class Manager(QtCore.QObject):
+    dataReady = QtCore.Signal(QtCore.QPointF, name='dataReady')
 
-from PySide2.QtCore import QUrl
+    def __init__(self, parent=None):
+        super(Manager, self).__init__(parent)
+        self._currX = 0
+        self._currY = 0
+        self._delay = 0.5
+        self._multiplier = 1.0
+        self._power = 1.0
+        self._xIncrement = 1.0
+        self._starter = False
+        self._goOn = False
+        self._threader = None
 
+    @QtCore.Property(bool)
+    def starter(self):
+        return self._starter
 
-app = QApplication([])
+    @starter.setter
+    def setStarter(self, val):
+        if self._multiplier == val:
+            return
+        print(val)
+        if val:
+            self.start()
+        else:
+            self.stop()
+        self._starter = val
 
-view = QQuickView()
+    @QtCore.Property(float)
+    def multiplier(self):
+        return self._multiplier
 
-url = QUrl("qml/MainQt.qml")
+    @multiplier.setter
+    def setMultiplier(self, val):
+        if self._multiplier == val:
+            return
+        print(val)
+        self._multiplier = val
 
+    @QtCore.Property(int)
+    def power(self):
+        return self._power
 
-view.setSource(url)
+    @power.setter
+    def setPower(self, val):
+        if self._power == val:
+            return
+        print(val)
+        self._power = val
 
-view.show()
+    @QtCore.Property(float)
+    def delay(self):
+        return self._delay
 
-app.exec_()
+    @delay.setter
+    def setDelay(self, val):
+        if self._delay == val:
+            return
+        print(val)
+        self._delay = val
+
+    @QtCore.Property(float)
+    def xIncrement(self):
+        return self._xIncrement
+
+    @xIncrement.setter
+    def setXIncrement(self, val):
+        if self._xIncrement == val:
+            return
+        print(val)
+        self._xIncrement = val
+
+    def generatePoint(self):
+        self._currX += self._xIncrement
+        self._currY = self._multiplier*(self._currX**self._power)
+
+        return self._currX,self._currY
+
+    def stop(self):
+        self._goOn = False
+        if self._threader is not None:
+            while self._threader.isRunning():
+                time.sleep(0.1)
+
+    def start(self):
+        self._goOn = True
+        self._threader = Threader(self.core, self)
+        self._threader.start()
+
+    def core(self):
+        while self._goOn:
+            p = QtCore.QPointF(*self.generatePoint())
+            self.dataReady.emit(p)
+            time.sleep(self._delay)
+
+# ------------------------------------------------- 
+
+class Threader(QtCore.QThread):
+    def __init__(self,core,parent=None):
+        super(Threader, self).__init__(parent)
+        self._core = core
+
+    def run(self):
+        self._core()
+
+if __name__ == "__main__":
+    os.environ["QT_QUICK_CONTROLS_STYLE"] = "Material"
+    app = QtWidgets.QApplication(sys.argv)
+    manager = Manager()
+    app.aboutToQuit.connect(manager.stop)
+    manager.start()
+    engine = QtQml.QQmlApplicationEngine()
+    ctx = engine.rootContext()
+    ctx.setContextProperty("Manager", manager)
+    engine.load('main.qml')
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    sys.exit(app.exec_())
